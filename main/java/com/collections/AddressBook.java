e8a6be8c2179da1b1973b2fa4e123706914a0e99
Naveen <naveentruly86@gmail.com>
2021-06-01 07:08:58 +0530

    Added: Create Contact of given inputs

--- main/java/com/collections/AddressBook.java
+++ main/java/com/collections/AddressBook.java
@@ -1,4 +1,27 @@
 package com.collections;
-
+import java.util.ArrayList;
 public class AddressBook {
-}
+        ArrayList<Contact> al;
+
+        //method to add contact to a list
+        public void addToList(Contact contact)
+        {
+            al = new ArrayList<>();
+            al.add(contact);
+        }
+
+        //method to get contact details
+        public void getContact()
+        {
+            System.out.println(al.get(0));
+        }
+
+        //main method
+        public static void main(String[] args)
+        {
+            Contact member = new Contact("Hello","There","Here it is","famous city","new state",543549,9848307659L,"hello@gmail.com");
+            AddressBook ab = new AddressBook();
+            ab.addToList(member);
+            ab.getContact();
+        }
+    }
